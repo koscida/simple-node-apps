52ec3e99734567f284d8ee6f2afa087b51022578
koscida <britkos@gmail.com>
2022-05-30 23:11:51 -0400

    adding newsletter app

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const https = require('https')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", (req, res) => {
+	const firstName = req.body.firstName
+	const lastName = req.body.lastName
+	const email = req.body.email
+	
+	const data = {
+		members: [
+			{
+				email_address: email,
+				email_type: 'html',
+				status: "subscribed",
+				merge_fields: {
+					FNAME: firstName,
+                	LNAME: lastName,
+				}
+			}
+		]
+	}
+	const jsonData = JSON.stringify(data)
+	console.log(jsonData)
+	
+	const dc="us9"
+	const listid="602f4e3b9b"
+	const apikey="1d97d1f262d6caf212bcd77d0942f525-us9"
+	
+	const url = "https://" + dc + ".api.mailchimp.com/3.0/lists/" + listid
+	console.log(url)
+	
+	const auth = 'apikey:' + apikey
+	
+	const options = {
+		method: 'POST',
+		auth: auth,
+		Authorization: auth
+	}
+	
+	const request = https.request(url, options, (response) => {
+		
+		if(response.statusCode === 200) {
+			res.sendFile(__dirname + "/success.html")
+		} else {
+			res.sendFile(__dirname + "/failure.html")
+		}
+		
+		response.on('data', (data) => {
+			const parsedData = JSON.parse(data)
+			console.log(parsedData)
+		})
+	})
+	request.write(jsonData)
+	request.end()
+})
+
+app.listen(3000, () => {
+	console.log("Server running on port 3000")
+})
+
+
+// mailchimp
+// api key: 1d97d1f262d6caf212bcd77d0942f525-us9
+// audience list id: 602f4e3b9b
+
